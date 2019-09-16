5c0f18ba1ec81ca8ee0fac02f6dbf9a96ed8115a
Mvejr <tyler.jones1@my.jcu.edu.au>
2019-09-16 22:56:25 +1000

    Finish most prac tasks

---
+++ Prac_06/guitar_test.py
@@ -0,0 +1,11 @@
+from prac_06.guitar import Guitar
+
+def run_tests():
+    name = "Gibson L-5"
+    year = 1922
+    cost = 16035.40
+
+    guitar = Guitar(name, year, cost)
+    other = Guitar("another Guitar", 2012, 1512.9)
+
+    print({}, get_age(), )
